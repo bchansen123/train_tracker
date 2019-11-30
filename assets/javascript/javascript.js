79bcd97239d1e36e1acf8ce4d29ded3d13bf0708
bchansen <bchansen@live.com>
2019-11-30 12:11:15 -0700

    Added next arrival math and minutes away math

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -18,15 +18,19 @@ firebase.initializeApp(config);
   //Submission button of new train
   $("#submitButton").on("click", function (event){
     event.preventDefault();
+
+
+
     var trainName = $("#trainName").val().trim();
     var trainDestination = $("#trainDestination").val().trim();
     var trainTime = $("#trainTime").val().trim();
     var trainFrequency = $("#trainFrequency").val().trim();
-
-    console.log(trainName);
-    console.log(trainDestination);
     console.log(trainTime);
-    console.log(trainFrequency);
+
+    // console.log(trainName);
+    // console.log(trainDestination);
+    // console.log(trainTime);
+    // console.log(trainFrequency);
     var newTrain = {
         name: trainName,
         destination: trainDestination,
@@ -42,13 +46,26 @@ firebase.initializeApp(config);
   });
 
   database.ref().on("child_added", function(trainSnap) {
-      console.log (trainSnap.val());
+
+    //Algorithm of converting time to moments to display the next arrival time and the minutes away
+    var firstTrnTime = trainSnap.val().time;
+    var convertingTrainTime = moment(firstTrnTime, "HH:mm").subtract(1, "years");
+    var trainTimeDiff = moment().diff(moment(convertingTrainTime),"minutes");
+    var timeArrival = trainTimeDiff % trainSnap.val().frequency;
+    var minutesArrival = trainSnap.val().frequency - timeArrival;
+    console.log(trainTimeDiff);
+    console.log(convertingTrainTime);
+    console.log(timeArrival);
+    var arrival = moment().add(minutesArrival, "minutes").format("hh:mm a");
+
       var trainName = trainSnap.val().name;
       var trainDestination = trainSnap.val().destination;
       var trainTime = trainSnap.val().time;
       var trainFrequency = trainSnap.val().frequency;
-      var arrivalTime ="";
-      var minutesAway = "";
+      var arrivalTime = arrival;
+      var minutesAway = minutesArrival;
+
+      //Appending new row to the table for data to be added
       var newValue = $("<tr>").append(
           $("<td>").text(trainName),
           $("<td>").text(trainDestination),
